77a370d21a50880e5c1546d04d3aab807179505c
ssaraaah <169175938+ssaraaah@users.noreply.github.com>
2024-06-06 19:32:33 +0200

    zadatak a

--- core.cpp
+++ core.cpp
@@ -32,7 +32,32 @@ int main(){
     for(int i = 0; i < broj_adaptera; i++){
         cout << niz[i] << " ";
     }
-    
+    // a:
+    int temp=0;
+    bool moguce_povezivanje;
+    for (int i = 0; i < broj_adaptera - 1; i++){
+            moguce_povezivanje = false;
+            for(int j = 0; j < broj_adaptera; j++){
+                if(niz[i]-temp<=3){
+                    moguce_povezivanje = true;
+                    temp=niz[i];
+                }
+            }
+            if(moguce_povezivanje == false){
+                break;
+            }
+    }
+
+    if (moguce_povezivanje == false){
+        cout << "Nije moguce povezivanje!!! " << endl;
+        exit(1);
+    }
+    else{
+        for(int i = 0; i < broj_adaptera; i++){
+        cout << niz[i] << " ";
+        }
+        cout << "Moguce povezivanje!"<<endl;
+    }
     
     return 0;
 }
